4525cc18ae826f8fd5d6be0836600a3f13d37a1f
jansan <jansan-26@hotmail.com>
2020-02-06 23:03:47 +0100

    tourData get and display

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -3,23 +3,24 @@ import './Tour.scss';
 
 class Tour extends Component {
     render() {
+        const { city, img, name, info } = this.props.tour;
         return (
             <article className="tour">
                 <div className="img-container">
-                    <img src="https://images.pexels.com/photos/3609211/pexels-photo-3609211.jpeg?cs=srgb&dl=golden-gate-bridge-san-francisco-california-3609211.jpg&fm=jpg" alt="img" />
+                    <img src={img} alt="img" />
                     <span className="close-btn">
                         <i className="fas fa-window-close"></i>
                     </span>
                 </div>
                 <div className="tour-info">
-                    <h3>city</h3>
-                    <h4>name</h4>
-                    <h5>info{""}<span className="fas fa-caret-square-down"></span></h5>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+                    <h3>{city}</h3>
+                    <h4>{name}</h4>
+                    <h5>info{" "}<span className="fas fa-caret-square-down"></span></h5>
+                    <p>{info}</p>
                 </div>
             </article>
         )
     }
 }
 
-export default Tour
+export default Tour;
