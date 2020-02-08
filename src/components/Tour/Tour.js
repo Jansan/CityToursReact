3c1314bb8e6ebb418f57d80370af99cfe70b3f64
jansan <jansan-26@hotmail.com>
2020-02-08 17:29:11 +0100

    TourList styled

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -2,6 +2,18 @@ import React, { Component } from 'react'
 import './Tour.scss';
 
 class Tour extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            showInfo: false
+        }
+    }
+    handleInfo = () => {
+        this.setState({
+            showInfo: !this.state.showInfo
+        })
+    }
     render() {
         const { city, img, name, info } = this.props.tour;
         return (
@@ -15,8 +27,12 @@ class Tour extends Component {
                 <div className="tour-info">
                     <h3>{city}</h3>
                     <h4>{name}</h4>
-                    <h5>info{" "}<span className="fas fa-caret-square-down"></span></h5>
-                    <p>{info}</p>
+                    <h5>
+                        info{" "}
+                        <span onClick={this.handleInfo}>
+                            <i className="fas fa-caret-square-down"></i></span></h5>
+                    {this.state.showInfo && <p>{info}</p>}
+
                 </div>
             </article>
         )
